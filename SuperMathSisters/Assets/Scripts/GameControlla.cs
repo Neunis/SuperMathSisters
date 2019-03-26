b079fe46c06a42917e64ceb43833f2200ce32dc6
Neunis <n_nunis@u.pacific.edu>
2019-03-25 20:37:49 -0700

    Timer now counts down, colors and display has changed

--- SuperMathSisters/Assets/Scripts/GameControlla.cs
+++ SuperMathSisters/Assets/Scripts/GameControlla.cs
@@ -16,20 +16,20 @@ public class GameControlla : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Timer.timeLeft <= 0)
+        /*if(Timer.timeLeft <= 0)
         {
             Time.timeScale = 0;
             timeLeft.gameObject.SetActive(true);
             //connect game level to restart screen
-        }
+        }*/
     }
 
-    public void restartScreen() // what is this function for -k
+    public void RestartScreen() // what is this function for -k
     {
         timeLeft.gameObject.SetActive(false);
         //restart load screen appears
         Time.timeScale = 1;
-        Timer.timeLeft = 10f;
+        //Timer.timeLeft = 10f;
         //for testing purposes
         SceneManager.LoadScene("Main Level"); 
     }
--- SuperMathSisters/Assets/Scripts/Timer.cs
+++ SuperMathSisters/Assets/Scripts/Timer.cs
@@ -6,69 +6,43 @@ using System;
 
 public class Timer : MonoBehaviour
 {
-    public Text timerText;
-    private float startTime;
-    private float endTime;
-    //currently set to 10 seconds for testing purposes
-    public static float timeLeft = 30f;
-    private bool completed = false;
+    //serialized field is the same as private however it also allow it to be seen in editor w/in Unity
+    [SerializeField] Text countdownTxt;
 
-    // Start is called before the first frame update
+    float currentTime = 0f;
+    float startTime = 30f;
+
+    bool counted;
+
     void Start()
     {
-        //timerText = GetComponent<Text>(); // this line was cause a null reference see https://answers.unity.com/questions/1352355/null-reference-exception-to-a-non-null-text-object.html
-        startTime = Time.time;
+        currentTime = startTime;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (completed)
-            return;
+        //time is decreade not on every frame but by Time fxn
+        currentTime -= 1 * Time.deltaTime;
+        countdownTxt.text = currentTime.ToString("0");
 
-        //this gives us the amount of time since time was started
-        float t = Time.time - startTime;
-        //this removes decimals and provides minutes
-        string minutes = ((int) t / 60).ToString();
-        //same principle as minutes but with seconds
-        //f2 within () just defines that only 2 decimals will be shown as a float
-        string seconds = (t % 60).ToString("f2");
-
-        if (timerText != null) { // check to see if the object is null or not
-            if (Convert.ToInt32(minutes) > 1.0 ) // if there is minute in the timer, then display it otherwise dont
-            {
-                timerText.text = minutes + ":" + seconds;
-
-            }
-            else
-            {
-                timerText.text = seconds;
-
-            }
+        if (currentTime >= 10)
+        {
+            countdownTxt.color = Color.black;
         }
         else
         {
-            Debug.Log("Timer is null");
+            countdownTxt.color = Color.red;
         }
-        /* timeLeft -= Time.deltaTime;
-         if (timeLeft < 0)
-         {
-             timeLeft = 0;
-             timerText.text = "Time Left: " + Mathf.Round(timeLeft);
-         }
-         */
 
-    }
-
-    public void EndResult()
-    {
-        completed = true;
-        timerText.color = Color.yellow;
+        if(currentTime <= 0)
+        {
+            currentTime = 0;
+        }
 
     }
 
     public bool GetCompleted() // I use this function to get the status the timer. if true then the time is up was my understanding. 
     {
-        return completed;
+        return counted;
     }
 }
