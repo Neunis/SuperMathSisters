a09aff7ba6443c7740ecb0b3a520756982e4d1df
Neunis <n_nunis@u.pacific.edu>
2019-04-03 20:39:24 -0700

    Main Menu Script added with basic features to play and quit, build setting adjusted for index order

---
+++ SuperMathSisters/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        //this line allows scenes to be loaded based on an index instead of 
+        //manually telling it which scenes to load
+        //order is already set up in build settings
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        //tester to see if game quits since application.quit only fxns once built
+        //for unity editor testing purposes
+        //TODO:DELETE ONCE COMPLETED
+        Debug.Log("Game Quitting"); 
+        Application.Quit();
+    }
+
+    
+}
