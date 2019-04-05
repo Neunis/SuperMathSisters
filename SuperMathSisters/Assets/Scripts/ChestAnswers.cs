f0114f4b9d071eed567f08b560e84b4c19afb867
Neunis <n_nunis@u.pacific.edu>
2019-04-05 00:12:52 -0700

    Shading upon Selection can occur with mouse

---
+++ SuperMathSisters/Assets/Scripts/ChestAnswers.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChestAnswers : MonoBehaviour
+{
+    //what allows you to press button
+    public KeyCode Key;
+    //private var created to connect keycode and button
+    private Button ChestAnswer;
+
+    //private reference to UI button
+    void Awake()
+    {
+        ChestAnswer = GetComponent<Button>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(Key))
+        {
+            OutlineColorSelection(ChestAnswer.colors.pressedColor);
+            //Click the button
+            ChestAnswer.onClick.Invoke();
+        }
+        else if (Input.GetKeyUp(Key))
+        {
+            OutlineColorSelection(ChestAnswer.colors.normalColor);
+        }
+    }
+
+    //usability feature that outlines selection for user respectively
+
+    void OutlineColorSelection(Color color)
+    {
+        Graphic graphic = GetComponent<Graphic>();
+        graphic.CrossFadeColor(color, ChestAnswer.colors.fadeDuration, true, true);
+    }
+}
+
+
