a49c68b8151e939effeec9ff5beb915da4aabcc7
Neunis <n_nunis@u.pacific.edu>
2019-04-05 17:11:04 -0700

    adjusted material and chest

--- SuperMathSisters/Assets/Scripts/ChestAnswers.cs
+++ SuperMathSisters/Assets/Scripts/ChestAnswers.cs
@@ -5,36 +5,39 @@ using UnityEngine.UI;
 
 public class ChestAnswers : MonoBehaviour
 {
-    //what allows you to press button
-    public KeyCode Key;
     //private var created to connect keycode and button
-    private Button ChestAnswer;
+    private Button CorrectChestAnswer;
+    private Button WrongChestAnswer;
 
     //private reference to UI button
     void Awake()
     {
-        ChestAnswer = GetComponent<Button>();
+        CorrectChestAnswer = GetComponent<Button>();
+        WrongChestAnswer = GetComponent<Button>();
     }
-
-    bool chosen = false;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Return))
         {
-            OutlineColorSelection(ChestAnswer.colors.pressedColor);
-           //ChestAnswer.onClick.Invoke();
+            OutlineColorSelection(CorrectChestAnswer.colors.pressedColor);
+            
         }
-            OutlineColorSelection(ChestAnswer.colors.normalColor);
-     
+        else
+        {
+            OutlineColorSelection(CorrectChestAnswer.colors.normalColor);
+        }
+
     }
 
+
+    //Start Couroutine();
 
     //usability feature that outlines selection for user respectively
     void OutlineColorSelection(Color color)
     {
         Graphic graphic = GetComponent<Graphic>();
-        graphic.CrossFadeColor(color, ChestAnswer.colors.fadeDuration, true, true);
+        graphic.CrossFadeColor(color, CorrectChestAnswer.colors.fadeDuration, true, true);
     }
 }
 
